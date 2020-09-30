6733b1094c57bb9d6e1e5e4a415fd4e2d0878d99
Spencer Mitchell <smitchell556@gmail.com>
2020-09-29 20:00:51 -0400

    Bump version

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/get-pass.scrbl" ())))
 (define pkg-desc "Utility to get passwords from the command line without echoing input.")
-(define version "0.0.1")
+(define version "0.0.2")
 (define pkg-authors '(Spencer Mitchell))
